0c87bcbe30eccec77e9dccaeaa9d2d7bc3a5baba
youyouW <675856128@qq.com>
2017-10-31 01:29:58 +0800

    登录功能完成了

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2017/10/31.
  */
+$(document).ajaxStart(function () {
+    //让进度条显示出来
+    NProgress.start();
+})
+
+
+$(document).ajaxStop(function () {
+    setTimeout(function () {
+        //让进度条结束
+        NProgress.done();
+    }, 500);
+});
