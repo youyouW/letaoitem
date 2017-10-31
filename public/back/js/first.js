a5b3eaf7d8a2eb9705f584a4818baaa3141d4e82
youyouW <675856128@qq.com>
2017-10-31 21:35:15 +0800

    一级分类功能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -31,7 +31,7 @@ $(function () {
                         currentPage = page;
                         render();
                     }
-                    })
+                });
 
             }
         })
