6a0d5118e965f9fcbc267e4fc33858028721b1e5
youyouW <675856128@qq.com>
2017-11-04 22:08:27 +0800

    个人中心页面完成

---
+++ public/front/js/user.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 2017/11/4.
+ */
+$(function () {
+    //1.渲染个人中心
+    $.ajax({
+        type:"get",
+        urll:"user/queryUserMessage",
+        success:function (data) {
+            tools.checkLogin(data);
+            $(".userinfo").html(template("tpl",data));
+            
+        }
+    });
+    
+    //2.退出功能
+    $(".logout a").on("click",function () {
+      mui.confirm("确定要退出系统吗？","提示",["是","否"],function (e) {
+          if(e.index === 1){
+              mui.toast("操作取消");
+          }else {
+              $.ajax({
+                  type:"get",
+                  url:"/user/logout",
+                  success:function(data){
+                      if(data.success){
+                          location.href ="login.html";
+                      }
+                  }
+              })
+          }
+      })  ;
+    });
+});
